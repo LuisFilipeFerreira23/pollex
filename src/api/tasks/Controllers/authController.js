8e5d03e85704ccd22e47b57324fcf9f132f6f3b5
tiagopaulorocha <tiago.rocha.roriz@gmail.com>
2025-10-30 20:30:56 +0000

    Fix Tasks

--- src/api/tasks/Controllers/authController.js
+++ src/api/tasks/Controllers/authController.js
@@ -1,12 +1,14 @@
 export function initialTesting(req, res, next) {
   res.status(200).json({ message: "Login route is working!" });
 }
-export function login() {
+export function login(req, res) {
+  res.send("123");
+}
+
+export function register(req, res) {
   console.log(123);
 }
-export function register() {
-  console.log(123);
-}
-export function passwordRecovery() {
+
+export function passwordRecovery(req, res) {
   console.log(123);
 }
